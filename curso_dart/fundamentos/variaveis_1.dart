d5a1cb47134495e54f9b368f4ffdd05fa507a11a
David Robinson <david@Davids-Mac-mini.local>
2020-09-24 21:30:18 -0300

    primeiros passos

---
+++ curso_dart/fundamentos/variaveis_1.dart
@@ -0,0 +1,11 @@
+main() {
+  
+  int a = 2;
+  double b = 2.5;
+
+  print(a);
+  print(b);
+
+  print(a*b);
+
+}
---
+++ curso_dart/fundamentos/variaveis_2.dart
@@ -0,0 +1,11 @@
+main() {
+
+var n1 = 2;
+var n2 = 1.5;
+var t1 = 'Olá Dart!';
+
+print(n1.runtimeType);
+print(n2.runtimeType);
+print(t1.runtimeType);
+
+}
